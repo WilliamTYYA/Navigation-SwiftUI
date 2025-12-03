3a3e80824df8937f2db419eb886cd6ac57ac2fea
Thiha Ye Yint Aung <thihayeyintaung110919@gmail.com>
2025-12-02 20:03:25 -0600

    Initial Commit

---
+++ Navigation-SwiftUI/Navigation_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Navigation_SwiftUIApp.swift
+//  Navigation-SwiftUI
+//
+//  Created by Thiha Ye Yint Aung on 12/2/25.
+//
+
+import SwiftUI
+
+@main
+struct Navigation_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Navigation-SwiftUITests/Navigation_SwiftUITests.swift
@@ -0,0 +1,16 @@
+//
+//  Navigation_SwiftUITests.swift
+//  Navigation-SwiftUITests
+//
+//  Created by Thiha Ye Yint Aung on 12/2/25.
+//
+
+import Testing
+
+struct Navigation_SwiftUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
